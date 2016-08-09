22796dc0b06b4883f5b05cbe782c0573e48a353f
Francisco <fraan.mp@gmail.com>
2016-08-09 11:16:03 -0300

    Email andando de nuevo

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -26,7 +26,7 @@ class ContactController extends Controller
         if ($validator -> passes()){
 
         \Mail::send('emails.template', $data, function ($message) {
-            $message->from('fraan.mp@gmail.com', 'Haras Online');
+            $message->from('info@harasonline.com', 'Haras Online');
 
             $message->to('fraan.mp@gmail.com')->subject('Mensaje de Harasonline.com');
         });
